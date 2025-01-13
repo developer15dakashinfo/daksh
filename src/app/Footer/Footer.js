e53fd0c08afd735707cc48e5ffb676ea2b4d8835
Aditi Rao <raoaditi1211@gmail.com>
2025-01-13 17:37:39 +0530

    new changes

--- src/app/Footer/Footer.js
+++ src/app/Footer/Footer.js
@@ -1,115 +1,205 @@
+import React, { useEffect, useState } from "react";
+import "./Footer.scss";
 
-import React from 'react'
-import Image from 'next/image';
-import "./Footer.scss"
 const Footer = () => {
+  const words = ["Fast", "Smart", "Diligent", "Focused"];
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 2000);
 
-    return (
-      <>
-      <footer className=''>
-        <div className="bg-black  py-4 text-gray-400">
-          <div className="container px-4 mx-auto">
-            <div className="-mx-4 flex flex-wrap justify-between">
-              <div className="px-4 my-4 ">
-              
-      <div className="scrolling-words-container">
-        <div className="scrolling-words-box">
-          <ul className='align-center'>
-            <li style={{color: '#ea4335'}}>Code</li>
-            <li style={{color: '#4285f4'}}>Build</li>
-            <li style={{color: '#34a853'}}>Create</li>
-            <li style={{color: '#fbbc04'}}>Design</li>
-            <li style={{color: '#ea4335'}}>Code</li>
-          </ul>
-        </div>
-        <span style={{color: '#5f6368'}}>for <br/>everyone</span>
-      </div>
-              </div>
-              <div className="px-4 my-4 w-full sm:w-auto">
-                <div>
-                  <h2 className="inline-block text-2xl pb-4 mb-4 border-b-4 border-blue-600">Company</h2>
+    return () => clearInterval(interval);
+  }, [words.length]);
+
+
+  const usefulLinks = [
+    "About Us",
+    "Blogs",
+    "Careers",
+    "Contact Us",
+    "Terms & Conditions",
+    "Privacy Policy",
+    "Refund Policy",
+  ];
+  const webDevelopmentLinks = [
+    "E-Commerce Website Design",
+    "WordPress Development Services",
+    "Graphic Design Services",
+  ];
+
+  const digitalMarketing = [
+    "Digital Marketing",
+    "SEO Service",
+    "SMO Service",
+    "Email Marketing",
+  ];
+
+  const mobileMarketing = ["Voice Call", "IVR", "Short/Long Code"];
+  const appDevelopmentLinks = [
+    "Android App Development",
+    "Hybrid App Development",
+    "iOS App Development",
+  ];
+
+  const renderLinks = (links) =>
+    links.map((link, index) => (
+      <li key={index} className="group relative">
+        <a href="#" className="relative inline-block">
+          {link}
+          <span className="absolute left-0 right-0 bottom-0 h-[1px] bg-white scale-x-0 origin-center transition-transform duration-500 group-hover:scale-x-100"></span>
+        </a>
+      </li>
+    ));
+
+  return (
+    <>
+      <footer className="bg-black py-4 text-gray-400">
+        <div className="container px-4 mx-auto">
+          <div className="-mx-4 flex flex-wrap justify-between">
+            {/* <div className="px-4 my-4">
+              <div className="scrolling-words-container">
+                <div className="scrolling-words-box">
+                  <ul className="align-center">
+                    <li className="text-red-500">Code</li>
+                    <li className="text-blue-500">Build</li>
+                    <li className="text-green-500">Create</li>
+                    <li className="text-yellow-500">Design</li>
+                    <li className="text-red-500">Code</li>
+                  </ul>
                 </div>
-                <ul className="leading-8">
-                  <li><a href="#" className="hover:text-blue-400">About Us</a></li>
-                  <li><a href="#" className="hover:text-blue-400">Terms &amp; Conditions</a></li>
-                  <li><a href="#" className="hover:text-blue-400">Privacy Policy</a></li>
-                  <li><a href="#" className="hover:text-blue-400">Contact Us</a></li>
+                <div className="text-gray-400">
+                  <p className="pb-[1]">for</p> 
+                  <p>everyone</p>
+                </div>
+              </div>
+            </div> */}
+            {/* <div className="flex items-center justify-center">
+              <div className="text-center">
+                <h1 className="text-6xl font-bold text-white">
+                  <span className="text-white transition-transform duration-500">
+                    Be
+                  </span>
+                  <span
+                    className="text-purple-400 transition-all duration-1000 ease-in-out"
+                    key={currentIndex}
+                  >
+                    {words[currentIndex]}
+                  </span>
+                </h1>
+              </div>
+            </div> */}
+
+<div className="flex justify-center items-center py-8">
+            <div className="relative text-center">
+              <h1 className="text-6xl font-bold text-white">
+                <span
+                  className={`inline-block transition-transform duration-1000 ${
+                    currentIndex === 0 ? "translate-x-0" : "translate-x-full"
+                  }`}
+                >
+                  Be
+                </span>{" "}
+                {words.map((word, index) => (
+                  <span
+                    key={index}
+                    className={`inline-block mx-2 text-white transition-all duration-1000 ease-in-out ${
+                      index === currentIndex ? "text-purple-400" : "text-gray-500"
+                    }`}
+                  >
+                    {word}
+                  </span>
+                ))}
+              </h1>
+            </div>
+            </div>
+
+            <div className="px-4 my-4">
+              <h2 className="inline-block text-[14px] font-semibold text-white pb-1 mb-2 relative before:absolute before:w-full before:h-[2px] before:bottom-0 before:left-0 before:bg-gradient-to-r before:from-sky-400 before:to-emerald-600 before:rounded-md">
+                Useful Links
+              </h2>
+              <ul className="leading-7 text-[14px]">
+                {renderLinks(usefulLinks)}
+              </ul>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="px-4 my-4">
+                <h2 className="inline-block text-[14px] font-semibold text-white pb-1 mb-2 relative before:absolute before:w-full before:h-[2px] before:bottom-0 before:left-0 before:bg-gradient-to-r before:from-sky-400 before:to-emerald-600 before:rounded-md">
+                  Web Development
+                </h2>
+                <ul className="leading-7 text-[14px]">
+                  {renderLinks(webDevelopmentLinks)}
                 </ul>
               </div>
-              <div className="px-4 my-4 w-full sm:w-auto">
-                <div>
-                  <h2 className="inline-block text-2xl pb-4 mb-4 border-b-4 border-blue-600">Company</h2>
-                </div>
-                <ul className="leading-8">
-                  <li><a href="#" className="hover:text-blue-400">About Us</a></li>
-                  <li><a href="#" className="hover:text-blue-400">Terms &amp; Conditions</a></li>
-                  <li><a href="#" className="hover:text-blue-400">Privacy Policy</a></li>
-                  <li><a href="#" className="hover:text-blue-400">Contact Us</a></li>
+
+              <div className="px-4 my-3">
+                <h2 className="inline-block text-[14px] font-semibold text-white pb-1 mb-2 relative before:absolute before:w-full before:h-[2px] before:bottom-0 before:left-0 before:bg-gradient-to-r before:from-sky-400 before:to-emerald-600 before:rounded-md">
+                  Digital Marketing
+                </h2>
+                <ul className="leading-7 text-[14px]">
+                  {renderLinks(digitalMarketing)}
                 </ul>
               </div>
-              <div className="px-4 my-4 w-full sm:w-auto">
-                <div>
-                  <h2 className="inline-block text-2xl pb-4 mb-4 border-b-4 border-blue-600">Blog</h2>
-                </div>
-                <ul className="leading-8">
-                  <li><a href="#" className="hover:text-blue-400">Getting Started With HTML and CSS</a></li>
-                  <li><a href="#" className="hover:text-blue-400">What Is Flex And When to Use It?</a></li>
-                  <li><a href="#" className="hover:text-blue-400">How TailwindCSS Can Help Your Productivity?</a></li>
-                  <li><a href="#" className="hover:text-blue-400">5 Tips to Make Responsive Website</a></li>
-                  <li><a href="#" className="hover:text-blue-400">See More</a></li>
+            </div>
+
+            <div className="">
+              <div className="px-4 my-4">
+                <h2 className="inline-block text-[14px] font-semibold text-white pb-1 mb-2 relative before:absolute before:w-full before:h-[2px] before:bottom-0 before:left-0 before:bg-gradient-to-r before:from-sky-400 before:to-emerald-600 before:rounded-md">
+                  App Development
+                </h2>
+                <ul className="leading-7 text-[14px]">
+                  {renderLinks(appDevelopmentLinks)}
                 </ul>
               </div>
-              <div className="px-4 my-4 w-full sm:w-auto xl:w-1/5">
-                <div>
-                  <h2 className="inline-block text-2xl pb-4 mb-4 border-b-4 border-blue-600">Connect With Us</h2>
-                </div>
-                <a href="#" className="inline-flex items-center justify-center h-8 w-8 border border-gray-100 rounded-full mr-1 hover:text-blue-400 hover:border-blue-400">
-                  <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-                    <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
-                  </svg>
-                </a>
-                <a href="#" className="inline-flex items-center justify-center h-8 w-8 border border-gray-100 rounded-full mr-1 hover:text-blue-400 hover:border-blue-400">
-                  <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                    <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
-                  </svg>
-                </a>
-                <a href="#" className="inline-flex items-center justify-center h-8 w-8 border border-gray-100 rounded-full mr-1 hover:text-blue-400 hover:border-blue-400">
-                  <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
-                  </svg>
-                </a>
-                <a href="#" className="inline-flex items-center justify-center h-8 w-8 border border-gray-100 rounded-full mr-1 hover:text-blue-400 hover:border-blue-400">
-                  <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
-                    <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
-                  </svg>
-                </a>
-                <a href="#" className="inline-flex items-center justify-center h-8 w-8 border border-gray-100 rounded-full hover:text-blue-400 hover:border-blue-400">
-                  <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                    <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" />
-                  </svg>
-                </a>
+
+              <div className="px-4 my-4">
+                <h2 className="inline-block text-[14px] font-semibold text-white pb-1 mb-2 relative before:absolute before:w-full before:h-[2px] before:bottom-0 before:left-0 before:bg-gradient-to-r before:from-sky-400 before:to-emerald-600 before:rounded-md">
+                  Mobile Marketing
+                </h2>
+                <ul className="leading-7 text-[14px]">
+                  {renderLinks(mobileMarketing)}
+                </ul>
               </div>
+            </div>
+
+            <div className="px-4 my-4 xl:w-1/5">
+              <h2 className="inline-block text-[14px] font-semibold text-white pb-1 mb-2 relative before:absolute before:w-full before:h-[2px] before:bottom-0 before:left-0 before:bg-gradient-to-r before:from-sky-400 before:to-emerald-600 before:rounded-md">
+                Connect With Us
+              </h2>
+              <address>
+                <p className="text-[14px]">
+                  <span className="font-bold text-white">Office: </span>
+                  G-2, G.S. Apartment, Near Sunshine Honda Showroom, Jaipur,
+                  Rajasthan.
+                </p>
+                <p className="text-[14px]">
+                  <span className="font-bold text-white">Call Us: </span>
+                  +91-9983388855, +91-9214292142, +91-9610106106, +91-7377371234
+                </p>
+                <p className="text-[14px]">
+                  <span className="font-bold text-white">Email Us: </span>
+                  info@dakshinfo.com, support@dakshinfo.com
+                </p>
+                <p className="text-[14px]">
+                  <span className="font-bold text-white">Kolkata Office: </span>
+                  76, 69, Diamond Harbour Rd, Barisha Behala-Kolkata, West
+                  Bengal 700008
+                </p>
+              </address>
             </div>
           </div>
         </div>
-        <div className="bg-sky-700 py-4 text-gray-100">
-          <div className="container mx-auto px-4">
-            <div className="-mx-4 flex flex-wrap justify-between">
-              <div className="px-4 w-full text-center sm:w-auto sm:text-left">
-                Copyright © 2024
-                - 2025
-                . All Rights Reserved.
-              </div>
-              <div className="px-4 w-full text-center sm:w-auto sm:text-left">
-              
-              </div>
-            </div>
-          </div>
+        <div className="px-8">
+          <hr className=" h-[1px] text-gray-500  " />
+          <p className="text-[14px] text-center pt-3">
+            © 2025 Daksh Infosoft Pvt. Ltd. | All Rights Reserved
+          </p>
         </div>
       </footer>
-      </>)
+    </>
+  );
 };
 
 export default Footer;
