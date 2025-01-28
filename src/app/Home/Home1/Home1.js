5cd45cbb336a67f6adb284ec1c50ae34063427ee
daksh-infosoft <developer15.dakashinfo@gmail.com>
2025-01-28 11:22:24 +0530

    new updates

--- src/app/Home/Home1/Home1.js
+++ src/app/Home/Home1/Home1.js
@@ -113,13 +113,7 @@ const ParallaxImages = () => {
         end={200}
         className="ml-auto w-1/3"
       />
-      <ParallaxImg
-        src="https://images.unsplash.com/photo-1494022299300-899b96e49893?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Orbiting satellite"
-        start={0}
-        end={-500}
-        className="ml-24 w-5/12"
-      />
+     
     </div>
   );
 };
